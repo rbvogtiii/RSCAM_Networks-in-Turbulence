6e7d74c9e65aa39ff76aad02f0d6a5bb3bcf43e4
Bradley Vogt <121974654+rbvogtiii@users.noreply.github.com>
2026-03-10 11:23:47 +0000

    added logic for A and G

--- viz.py
+++ viz.py
@@ -5,14 +5,12 @@ dk=0.5;kmax=30;kmin=6
 n=int(kmax/dk)*2+1
 idk=dk*(np.array(np.where(np.zeros([n]*2)==0))-(kmax/dk)).astype(int)
 order=lambda x: ((x[0]+kmax)/dk)*n+((x[1]+kmax)/dk)
-kmaxx=kmax**2
-kminn=kmin**2
 
 k=[];p=[];q=[]
 
 for i in range(len(idk.T)):
-    tmp1=(np.linalg.norm(idk.T[i].reshape(2,1),axis=0)**2<=kmaxx) & (np.linalg.norm(idk,axis=0)**2<=kmaxx) & (np.linalg.norm(-idk-idk.T[i].reshape(2,1),axis=0)**2<=kmaxx)
-    tmp2=(np.linalg.norm(idk.T[i].reshape(2,1),axis=0)**2>=kminn) & (np.linalg.norm(idk,axis=0)**2>=kminn) & (np.linalg.norm(-idk-idk.T[i].reshape(2,1),axis=0)**2>=kminn)
+    tmp1=(np.linalg.norm(idk.T[i].reshape(2,1),axis=0)<=kmax) & (np.linalg.norm(idk,axis=0)<=kmax) & (np.linalg.norm(-idk-idk.T[i].reshape(2,1),axis=0)<=kmax)
+    tmp2=(np.linalg.norm(idk.T[i].reshape(2,1),axis=0)>=kmin) & (np.linalg.norm(idk,axis=0)>=kmin) & (np.linalg.norm(-idk-idk.T[i].reshape(2,1),axis=0)>=kmin)
     tmp3=(order(idk.T[i])<order(idk)) & (order(idk)<order(-idk-idk.T[i].reshape(2,1)))
     if (tmp1&tmp2&tmp3).sum()!=0:
         k.append(np.array([idk.T[i]]*(tmp1&tmp2&tmp3).sum()))
@@ -23,7 +21,16 @@ for i in range(len(idk.T)):
 k=np.row_stack(k).T;p=np.row_stack(p).T;q=np.row_stack(q).T
 A=np.zeros([len(idk.T)]*2);G=np.zeros([len(idk.T)]*2)
 A[order(k).astype(int),order(p).astype(int)]=1
+A[order(k).astype(int),order(q).astype(int)]=1
+A[order(p).astype(int),order(q).astype(int)]=1
+A=A+A.T
+
 G[order(k).astype(int),order(p).astype(int)]=(np.linalg.norm(k,axis=0)**2)*np.cross(q,p,axis=0)
+G[order(k).astype(int),order(q).astype(int)]=(np.linalg.norm(k,axis=0)**2)*np.cross(p,q,axis=0)
+G[order(p).astype(int),order(k).astype(int)]=(np.linalg.norm(p,axis=0)**2)*np.cross(q,k,axis=0)
+G[order(p).astype(int),order(q).astype(int)]=(np.linalg.norm(p,axis=0)**2)*np.cross(k,q,axis=0)
+G[order(q).astype(int),order(k).astype(int)]=(np.linalg.norm(q,axis=0)**2)*np.cross(p,k,axis=0)
+G[order(q).astype(int),order(p).astype(int)]=(np.linalg.norm(q,axis=0)**2)*np.cross(k,p,axis=0)
 
 print('done!')
 
@@ -36,14 +43,13 @@ fig,ax=plt.subplots(layout='constrained')
 im=ax.imshow(conn,extent=[-kmax,kmax,-kmax,kmax],origin='lower',cmap='plasma')
 plt.colorbar(im)
 plt.savefig('./img/heatmap.png',dpi=300,bbox_inches='tight')
-plt.show()
 
 conn=np.zeros([n]*2)
 tmp=((idk+kmax)/dk).round().astype(int)
-conn[tmp[0],tmp[1]]=G.sum(axis=1)
+conn[tmp[0],tmp[1]]=np.abs(G).sum(axis=1)
 
 fig,ax=plt.subplots(layout='constrained')
-im=ax.imshow(conn,extent=[-kmax,kmax,-kmax,kmax],origin='lower')
+im=ax.imshow(conn,extent=[-kmax,kmax,-kmax,kmax],origin='lower',cmap='plasma')
 plt.colorbar(im)
 plt.savefig('./img/heatmap2.png',dpi=300,bbox_inches='tight')
 plt.show()
